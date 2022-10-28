d683916240258a6a3c8b569597030998e13ba178
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-10-28 15:05:18 +0100

    added cors header

--- hngi9/settings.py
+++ hngi9/settings.py
@@ -49,14 +49,16 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
-    "corsheaders.middeleware.CorsMiddleware",
+   
+    
 ]
 
 ROOT_URLCONF = "hngi9.urls"
