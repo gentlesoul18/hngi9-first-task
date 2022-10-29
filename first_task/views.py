242ae3b56b029f5bf53c4e67fbbb924574a3a50c
Gentlesoul <oyebamijibarakat7@gmail.com>
2022-10-29 13:04:22 +0100

    changes

--- first_task/views.py
+++ first_task/views.py
@@ -2,6 +2,6 @@ from django.http import JsonResponse
 
 
 def details(request):
-    output = {"slackUsername": "Gentlesoul", "backend": True, "Age": 22, "bio": "Gentlesoul is a backend developer but note the small 'in-view' because i am still a beginner"}
+    output = {"slackUsername": "Gentlesoul", "backend": True, "age": 22, "bio": "Gentlesoul is a backend developer but note the small 'in-view' because i am still a beginner"}
 
     return JsonResponse(output)
